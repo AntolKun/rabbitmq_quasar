a7efb76437ab105e95402f0f30a9b0a7f0e4fc6f
AntolKun <aloysiusluilui@gmail.com>
2024-06-29 20:55:08 +0700

    ubah routing key ke milik pribadi

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-pa-md">
     <div class="q-gutter-md" style="max-width: 1500px">
-      <q-input filled type="textarea" rows="50" readonly v-model="message"/>
+      <q-input filled type="textarea" rows="50" readonly v-model="message" />
     </div>
   </div>
 </template>
@@ -13,7 +13,7 @@ import * as mqtt from "mqtt/dist/mqtt.min";
 export default {
   data() {
     return {
-      message: '',
+      message: "",
     };
   },
 
@@ -27,16 +27,19 @@ export default {
 
     const client = mqtt.connect("ws://103.167.112.188:15675/ws", opt);
     client.on("connect", () => {
-      this.message = 'Okeh sip, konek bos \n'
-    })
+      this.message = "Okeh sip, konek bos \n";
+    });
     client.stream.on("error", function (err) {
       if (err) {
         console.log("ini ngeleg ini");
       }
     });
     client.on("connect", function () {
-      client.subscribe("routing_edp", function () {
-        client.publish("routing_edp", `CINTAI USUSMU LIHAT TOLO TIAP HARI (INI DARI WEB) ${new Date().toString()}`);
+      client.subscribe("tolo-arif", function () {
+        client.publish(
+          "tolo-arif",
+          `CINTAI USUSMU LIHAT TOLO TIAP HARI (INI DARI WEB) ${new Date().toString()}`
+        );
       });
     });
     client.stream.on("error", function (err) {
@@ -45,9 +48,10 @@ export default {
       }
     });
     client.on("connect", function () {});
-    client.subscribe("routing_edp");
+    client.subscribe("tolo-arif");
     client.on("message", (topic, message) => {
       console.log(message.toString());
+
       this.message += `${message.toString()} \n`;
     });
   },
